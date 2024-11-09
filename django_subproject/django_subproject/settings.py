5f95434f59b0388f06fc0886bd111205cf8bc0bd
elinsky <dmitry.ilinsky@gmail.com>
2024-11-09 19:14:59 +0300

    django connected, tables created

--- django_subproject/django_subproject/settings.py
+++ django_subproject/django_subproject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orm_app',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,15 @@ WSGI_APPLICATION = 'django_subproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DJ_NAME'],
+        'USER': os.environ['DJ_USER'],
+        'PASSWORD': os.environ['DJ_PASS'],
+        'HOST': os.environ['DJ_HOST'],
+        'PORT': os.environ['DJ_PORT'],
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- django_subproject/orm_app/models.py
+++ django_subproject/orm_app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
+
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    orders = models.ManyToManyField("Order", through="OrderUser", related_name="users")
+
+
+class UserAddress(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.ForeignKey("Address", on_delete=models.CASCADE)
+
+
+class Address(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    zipcode = models.CharField(max_length=6)
+    street = models.CharField(max_length=100)
+    number = models.CharField(max_length=100)
+    apartment = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    users = models.ManyToManyField(User, through=UserAddress, related_name="addresses")
+
+
+class Group(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+
+
+class OrderUser(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_cost = models.FloatField()
+
 # Create your models here.
